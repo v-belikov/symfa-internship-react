ace61052352d97783b6462abd41174b578e69d2c
Eugene Kovgan <jugin.kovgan87@gmail.com>
2023-02-21 20:10:48 +0300

    react-shop-eugene: feat: add axious

--- src/app/components/gallery/gallery.tsx
+++ src/app/components/gallery/gallery.tsx
@@ -1,12 +1,19 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
+import { getProduct } from 'app/api/api';
 import { GalleryItem } from './gallery-item';
 
 export const Gallery: FC = () => {
+  // const [products, setProducts] = useState();
+
+  useEffect(() => {
+    getProduct();
+  }, []);
+
+  // console.log(products);
+
   return (
     <div className="gallery">
       <GalleryItem />
-      {/* <GalleryItem />
-      <GalleryItem /> */}
     </div>
   );
 };
